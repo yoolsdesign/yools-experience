b7b557acfde1f5ae38e03666c404ef02e711a973
Yools <25012951+yoolsdesign@users.noreply.github.com>
2018-05-03 14:26:03 +0200

    Add files via upload

---
+++ public/js/yools_backend.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function( $ ) {
+
+
+	/*
+	    *
+	    *
+	    * FLOATING BUTTON
+	    *
+	    *
+	*/
+
+	setTimeout(function(){
+	  $('.yools-floating-button .floating-message-1').addClass("active");
+	}, 0);
+
+	setTimeout(function(){
+	  $('.yools-floating-button .floating-message').removeClass("active");
+	}, 300000);
+
+	setTimeout(function(){
+	  $('.yools-floating-button .floating-message-2').addClass("active");
+	}, 300250);
+
+});

f8dab24a4b090aa527abcb22501b74d0bc72de81
Yools <25012951+yoolsdesign@users.noreply.github.com>
2018-05-08 14:09:54 +0200

    Update yools-experience.php

--- yools-experience.php
+++ yools-experience.php
@@ -3,7 +3,7 @@
 Plugin Name:  Yools Experience
 Plugin URI:   https://yools.be
 Description:  Yools Experience Plugin
-Version:      0.3.2
+Version:      0.3.1
 Author:       Yools
 Author URI:   https://yools.be
 */
